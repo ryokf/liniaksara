69beb933351969e2f09e747bbfc524d5931e09f5
ryokf <ryokhrisnaf@gmail.com>
2025-08-07 07:51:23 +0700

    refactor(layout): remove LayoutClientWrapper and SessionWrapper components for simplification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
         <AuthProvider>
-          <LayoutClientWrapper>{children}</LayoutClientWrapper>
+            {children}
         </AuthProvider>
       </body>
     </html>
